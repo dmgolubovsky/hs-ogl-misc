2f292353f51426b678eeacb7b1f3eba6edc8aee6
Dmitry Golubovsky <golubovsky@gmail.com>
2010-07-01 23:47:22 -0400

    Partial code for capri bootstrap command.

--- capri/Main.hs
+++ capri/Main.hs
@@ -17,7 +17,12 @@ module Main where
 
 import qualified Paths_capri
 import Data.List
+import System.Directory
+import System.FilePath
+import System.Process
 import System.Environment
+import System.Exit
+import Control.Monad
 import Distribution.Text
 import Distribution.Simple.Setup
 import Distribution.Simple.Utils
@@ -25,6 +30,9 @@ import Distribution.Simple.Command
 
 main :: IO ()
 main = getArgs >>= mainWorker
+
+-- Toplevel command handler. Any command not recognized in the provided list of
+-- commands is forwarded to cabal-install as is, with proper environment set.
 
 mainWorker :: [String] -> IO ()
 mainWorker args = topHandler $
@@ -38,7 +46,7 @@ mainWorker args = topHandler $
           | fromFlag (globalNumericVersion globalflags) -> printNumericVersion
         CommandHelp     help           -> printCommandHelp help
         CommandList     opts           -> printOptionsList opts
-        CommandErrors   errs           -> printErrors errs
+        CommandErrors   errs           -> processErrors args errs $ printErrors errs
         CommandReadyToGo action        -> action globalflags
   where
      printCommandHelp help = do
@@ -56,7 +64,24 @@ mainWorker args = topHandler $
                                    ++ display cabalVersion
                                    ++ " of the Cabal library "
 
-commands = []
+-- Process errors as they possibly mean just a command for Cabal. Otherwise
+-- do the default action.
+
+processErrors :: [String] -> [String] -> IO () -> IO ()
+
+processErrors args errs dflt = putStrLn (show errs) >> case errs of
+  [] -> dflt
+  (m:_) -> case words m of
+    ("unrecognised":"command:":_) -> cabalInstall args
+    _ -> dflt
+
+-- (TODO) Run the cabal executable with proper environment settings.
+
+cabalInstall :: [String] -> IO ()
+cabalInstall args = 
+  putStrLn $ "running Cabal with " ++ show args
+
+-- Print global help message
 
 capriGlobalCommand :: CommandUI GlobalFlags
 capriGlobalCommand = CommandUI {
@@ -84,5 +109,39 @@ capriGlobalCommand = CommandUI {
       ]
   }
 
+bootpkgs = ["ffi", "rts", "integer-gmp", "base-4*"]
 
+subdir = ".capri"
+pkgdir = subdir </> "packages"
+instdir = subdir </> "install"
+
+commands = [bootstrapCommand `commandAddAction` bootstrapAction]
+
+bootstrapCommand :: CommandUI ()
+
+bootstrapCommand = CommandUI {
+  commandName         = "bootstrap"
+ ,commandSynopsis     = "Bootstrap private packages configuration"
+ ,commandUsage        = (++ " bootstrap")
+ ,commandDescription  = Just $ \pname -> "Use this command to create an empty per-project " ++
+                                         "configuration of packages.\n" ++ pname ++ " will " ++
+                                         "clone the following packages into the per-project " ++
+                                         "packages directory: \n\n" ++
+                                         concat (intersperse " " bootpkgs) ++ "\n\n"
+ ,commandDefaultFlags = ()
+ ,commandOptions = const []
+}
+
+-- Bootstrap a private per-project package configuration. Create a subdirectory .capri
+-- where packages will be installed. Subdirectory .capri/packages will hold packages
+-- cache and descriptions; subdirectory .capri/install will hold compiled libraries and
+-- executables.
+
+bootstrapAction f s g = do
+  mapM createDirectory [subdir, instdir]
+  ex <- runCommand ("ghc-pkg init " ++ pkgdir) >>= waitForProcess
+  let exfail (ExitFailure _) = True
+      exfail _ = False
+  when (exfail ex) $ die "ghc-pkg failed to initialize package configuration:\n"
+  return ()
 
