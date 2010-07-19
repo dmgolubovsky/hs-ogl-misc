ddd471700eafa3742bde545a21f20fb314b99a58
Dmitry Golubovsky <golubovsky@gmail.com>
2010-07-19 07:29:22 -0400

    Initial code for per-process namespace support.

--- io-layer/System/IO9/NameSpace/Pure.hs
+++ io-layer/System/IO9/NameSpace/Pure.hs
@@ -17,16 +17,30 @@ module System.IO9.NameSpace.Pure (
   BoundDir (..)
  ,BindFlag (..)
  ,UnionDir (..)
+ ,NsValue (..)
+ ,DevEnt (..)
+ ,NameSpace
  ,unionDir
+ ,setGlobal
+ ,getGlobal
  ,bindDirAt) where
 
+import Data.Char
 import System.FilePath
+import Control.Monad
 import qualified Data.DList as DL
+import qualified Data.Map as M
 
 -- Need this to keep compiler happy.
 
 instance (Show a) => Show (DL.DList a) where
   show x = show (DL.toList x)
+
+instance (Ord a) => Ord (DL.DList a) where
+  compare x y = compare (DL.toList x) (DL.toList y)
+
+instance (Eq a) => Eq (DL.DList a) where
+  x == y = (DL.toList x) == (DL.toList y)
 
 -- | A datatype to represent a component of a bound directory. Lists of bound directories
 -- are stored in the namespace map as values.
@@ -34,7 +48,7 @@ instance (Show a) => Show (DL.DList a) where
 data BoundDir = BoundDir {
   dirfp :: FilePath                -- ^ Absolute path to the actual directory
  ,dircr :: Bool                    -- ^ 'True' if creation of files is allowed
-} deriving (Show)
+} deriving (Eq, Ord, Show)
 
 -- | A datatype to represent binding mode: before/after/replace, and
 -- the boolean flag to allow/prohibit creation of files in this actual directory
@@ -48,7 +62,7 @@ data BindFlag = BindBefore Bool    -- ^ Bind before any directory bound to this 
 
 -- | A datatype to hold bound directories in an ordered list.
 
-data UnionDir = UnionDir (DL.DList BoundDir) deriving (Show)
+data UnionDir = UnionDir (DL.DList BoundDir) deriving (Eq, Ord, Show)
 
 -- | Create an union directory containing a single directory. file creation
 -- is enabled by default.
@@ -67,5 +81,47 @@ bindDirAt (UnionDir dl) fp bf = case bf of
   BindBefore cr -> UnionDir $ DL.cons (BoundDir {dirfp = fp, dircr = cr}) dl
   BindAfter cr -> UnionDir $ DL.snoc dl (BoundDir {dirfp = fp, dircr = cr})
 
+-- | A datatype to represent a namespace key value.
 
+data NsValue = UnionPoint UnionDir      -- ^ A union point
+             | DeviceEntry Char DevEnt  -- ^ Device entry (host directory translation or 
+                                        -- a virtual device serving its file system (TBD).
+                                        -- A character in the entry defines the kernel namespace
+                                        -- letter (X in #X).
+             | GlobalSetting String     -- ^ A global setting (not to be confused 
+                                        -- with process environment)
+               deriving (Eq, Ord, Show)
+
+-- | Device entry. Two types of "devices" are recognized: host filesystem prefix translation,
+-- and a virtual device exporting its own file system (TBD).
+
+data DevEnt = HostPath FilePath         -- ^ Host filesystem path. If such entry is encountered
+                                        -- during a path evaluation, it is prefixed to the
+                                        -- evaluation result, and evaluation continues.
+          {-| DevTable ... - TBD -}
+              deriving (Eq, Ord, Show)
+
+
+-- | The namespace itself which is a map of strings to 'NsValue's. Namespace data is stored at
+-- thread-level (may be shared between several threads) in a mutable reference, so impure
+-- namespace functions just update those references as needed. Namespace keys start with '/' for
+-- filesystem paths, with '#' for kernel device entries, with '$' for global settings.
+
+type NameSpace = M.Map String NsValue
+
+-- | Update a global setting. New value is created in the map, existing value is updated.
+-- Do not prepend a '$' to the setting name (will be added automatically).
+
+setGlobal :: NameSpace -> String -> String -> NameSpace
+
+setGlobal ns k v = M.insert ('$':k) (GlobalSetting v) ns
+
+-- | Retrieve a global setting (its most recent value). Monadic failure occurs if there is no
+-- such setting. Do not prepend a '$' to the setting name.
+
+getGlobal :: (Monad m) => NameSpace -> String -> m String
+
+getGlobal ns k = case M.lookup ('$':k) ns of
+  Just (GlobalSetting s) -> return s
+  _ -> fail $ "getGlobal: $" ++ k ++ " does not exist"
 
