22e115c7c1f3e8a3b557e8c20799b97eb5cd0d7d
Dmitry Golubovsky <golubovsky@gmail.com>
2010-07-28 23:47:20 -0400

    Implemented the "stat" operation of 9P2000.

--- io-layer/System/IO9/Devices/DevPosix.hs
+++ io-layer/System/IO9/Devices/DevPosix.hs
@@ -24,6 +24,7 @@ import System.FilePath
 import System.Directory
 import System.IO9.Device
 import System.Posix.Files
+import System.Posix.User
 import Control.Concurrent
 import qualified Data.Map as M
 
@@ -85,11 +86,10 @@ dpvers devd msg = return $ Resp9P (errorMsg (msg_tag msg) $
 dpauth :: DevPosix -> Device9P
 
 dpauth devd msg@(Msg TTattach tg ta@Tattach {}) = do
-  let norm = normalise (hfp devd ++ tat_aname ta)
+  let norm = normalise (hfp devd ++ "/" ++ tat_aname ta)
   tree <- canonicalizePath norm 
-  ex1 <- doesFileExist tree
-  ex2 <- doesDirectoryExist tree
-  case isAbsolute (tat_aname ta) && (ex1 || ex2) && isSubdir (hfp devd) tree of
+  ex <- fileExist tree
+  case ex && isSubdir (hfp devd) tree of
     False -> return $ Resp9P (errorMsg (msg_tag msg) $ "Invalid or non-existent path " ++ 
                                                       tat_aname ta) (dpauth devd)
     True -> do
@@ -125,10 +125,45 @@ dpacc devd msg | msg_typ msg == TTflush =
   return $ Resp9P (msg {msg_typ = TRflush, msg_body = Rflush}) (dpacc devd) 
 
 dpacc devd msg = do
+  let emsg x = return $ Resp9P (errorMsg (msg_tag msg) x) (dpacc devd)
   tid <- myThreadId
   if (devshr devd) && isJust (thrid devd) && (tid /= fromJust (thrid devd)) 
     then dpvers (newdata (devshr devd) (hfp devd)) msg
-    else devError "got here..." msg
+    else case msg_typ msg of
+      TTstat -> do                  -- if file/dir does not exist, return an empty list of stats
+        let sfid = ts_fid $ msg_body msg
+            spath = M.lookup sfid $ fidmap devd
+        case spath of
+          Nothing -> emsg $ "Incorrect fid " ++ show sfid
+          Just sfp -> do
+            ex <- fileExist sfp     -- file could have disappeared
+            case ex of
+              False -> return $ Resp9P (msg {msg_typ = TRstat, msg_body = Rstat []}) (dpacc devd)
+              True -> do
+                st <- getFileStatus sfp
+                funame <- getUserEntryForID (fileOwner st) >>= return . userName
+                fgroup <- getGroupEntryForID (fileGroup st) >>= return . groupName
+                let qid = stat2qid st
+                    mode = stat2mode st
+                    fname = if normalise (sfp ++ "/") == normalise (hfp devd ++ "/") 
+                              then "/"
+                              else head $ reverse $ splitPath sfp
+                    ret = Stat {
+                      st_typ = 0    -- these are not filled in by the driver, but
+                     ,st_dev = 0    -- rather by the surrounding framework
+                     ,st_qid = qid
+                     ,st_mode = mode
+                     ,st_atime = round $ realToFrac $ accessTime st
+                     ,st_mtime = round $ realToFrac $ modificationTime st
+                     ,st_length = fromIntegral $ fileSize st
+                     ,st_name = fname
+                     ,st_uid = funame
+                     ,st_gid = fgroup
+                     ,st_muid = funame
+                    }
+                return $ Resp9P (msg {msg_typ = TRstat, msg_body = Rstat [ret]}) (dpacc devd)
+      _ -> emsg $ "Incorrect message " ++ show msg
+
     
 
 -- Check that the path2 is a subdirectory of path1 (or equal to path1).
@@ -158,5 +193,31 @@ stat2qid stat =
        ,qid_path = fromIntegral inode
       }
   in  qid
+
+-- Build a filemode mask in terms of the 9P definition.
+
+stat2mode :: FileStatus -> Word32
+
+stat2mode st =
+  let umode = fileMode st
+      oshift = 6
+      gshift = 3
+      wshift = 0
+      permmap = [(ownerReadMode, c_DMREAD `shiftL` oshift)
+                ,(ownerWriteMode, c_DMWRITE `shiftL` oshift)
+                ,(ownerExecuteMode, c_DMEXEC `shiftL` oshift)
+                ,(groupReadMode, c_DMREAD `shiftL` gshift)
+                ,(groupWriteMode, c_DMWRITE `shiftL` gshift)
+                ,(groupExecuteMode, c_DMEXEC `shiftL` gshift)
+                ,(otherReadMode, c_DMREAD `shiftL` wshift)
+                ,(otherWriteMode, c_DMWRITE `shiftL` wshift)
+                ,(otherExecuteMode, c_DMEXEC `shiftL` wshift)
+                ,(directoryMode, c_DMDIR)]
+      nmode = foldl mbit 0 permmap
+      mbit acc (umb, nmb) = case umb .&. umode of
+        0 -> acc
+        _ -> acc .|. nmb
+  in  fromIntegral nmode
+
 
 
